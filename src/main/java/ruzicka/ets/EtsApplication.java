157042c00aac8b4a22283d8fd2b7e9d4bbae6ff8
666hellmaster <annonymause@email.cz>
2024-09-30 21:01:20 +0200

    init

---
+++ src/main/java/ruzicka/ets/EtsApplication.java
@@ -0,0 +1,13 @@
+package ruzicka.ets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtsApplication.class, args);
+    }
+
+}
