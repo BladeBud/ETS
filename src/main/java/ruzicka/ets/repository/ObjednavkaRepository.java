524d2bf803dd965cc0848ca9673f80f0ea0c4bea
666hellmaster <annonymause@email.cz>
2024-10-06 23:01:58 +0200

    v2.1.3

--- src/main/java/ruzicka/ets/repository/ObjednavkaRepository.java
+++ src/main/java/ruzicka/ets/repository/ObjednavkaRepository.java
@@ -1,6 +1,7 @@
 package ruzicka.ets.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ruzicka.ets.db.Objednavka;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
  * @author czech
  * @since 2024-09-30
  */
-
+@Repository
 public interface ObjednavkaRepository extends JpaRepository<Objednavka, Integer> {
     List<Objednavka> findByIdzakaznik_Idzakaznik(Integer idzakaznik);
 
