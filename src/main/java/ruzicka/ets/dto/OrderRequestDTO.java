537fecd79949ea8023a1db6e794849702caf2022
666hellmaster <annonymause@email.cz>
2024-11-10 15:38:03 +0100

    Disable zakaznikId field in OrderRequestDTO
    Commented out the zakaznikId field and its corresponding getter and setter methods. This change is likely to mitigate issues related to this field in the current implementation while keeping the code structure for possible future use.

--- src/main/java/ruzicka/ets/dto/OrderRequestDTO.java
+++ src/main/java/ruzicka/ets/dto/OrderRequestDTO.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class OrderRequestDTO {
     private List<String> nazvy; // List of table names
     private List<Integer> quantities; // List of quantities for each table
-    private Integer zakaznikId;
+    //private Integer zakaznikId;
     private String mail;
 
     // Getters and Setters
@@ -25,13 +25,13 @@ public class OrderRequestDTO {
         this.quantities = quantities;
     }
 
-    public Integer getZakaznikId() {
-        return zakaznikId;
-    }
-
-    public void setZakaznikId(Integer zakaznikId) {
-        this.zakaznikId = zakaznikId;
-    }
+//    public Integer getZakaznikId() {
+//        return zakaznikId;
+//    }
+//
+//    public void setZakaznikId(Integer zakaznikId) {
+//        this.zakaznikId = zakaznikId;
+//    }
 
     public void setMail(String mail) {
         this.mail = mail;
